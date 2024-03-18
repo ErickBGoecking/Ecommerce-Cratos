7961944ca2f9ca3181c519334809e425dd7503c0
Fernandodigitalarte <fernandodigitalarte@gmail.com>
2024-03-18 20:13:54 -0300

    cliente início

---
+++ cliente/cliente.php
@@ -0,0 +1,18 @@
+<?php 
+include_once("cabecalho.php");
+?>
+
+<body class="bg-body-tertiary">
+
+    <div class="d-flex flex-column flex-md-row">
+
+        <?php include_once('barraLateral.php');?>
+
+        <div class="conteúdo col-md-10 bg-body-secondary" style="min-height: 100vh;">
+
+        </div>
+    </div>
+    
+</body>
+
+</html>
