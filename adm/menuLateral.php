1b016f6007902eeff0631a24905d1ca0ec4f3649
exclusivy <lcpyes@hotmail.com>
2024-03-18 20:44:44 -0300

    Topo e menuLateral e Cabeção e css padronizado Luciano e Nando

---
+++ adm/menuLateral.php
@@ -0,0 +1,80 @@
+
+<style>
+    .item-menu * {
+        color: slategray;
+        padding-left: 10px;
+    }
+
+    .item-menu :hover {
+        color: slategray !important;
+        border-left: 5px solid rgb(106, 90, 205);
+        margin-left: -5px;
+        cursor: pointer;
+    }
+
+    .item-menu * :hover {
+        color: slategray !important;
+        border-left: 0px;
+        margin-left: 0px;
+    }
+</style>
+
+<div class="barra_lateral col-md-2">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid align-items-start">
+            <a class="navbar-brand p-0 m-0" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse m-0 mt-3 " id="navbarTogglerDemo02">
+
+
+                <nav class="nav flex-column container-fluid justify-content-start p-0">
+
+                    <div class="accordion accordion-flush container-fluid  p-0" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <div class=" item-menu">
+                                <h2 class="accordion-header">
+                                    <div class="accordion-button collapsed bg-body-tertiary ps-0"
+                                         data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+                                         aria-expanded="false" aria-controls="flush-collapseOne">
+                                        <span class="mdi mdi-shopping"></span><span class="ps-4"></span>Compras
+                                    </div>
+                                </h2>
+                            </div>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                 data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+
+
+                                    <div class=" item-menu"><a class="nav-link" aria-current="page"
+                                                               href="./cliente-historico.php"><span class="mdi mdi-order-bool-descending"></span><span class="ps-4"></span>Histórico</span></a></div>
+                                    <div class=" item-menu"><a class="nav-link" aria-current="page" href="./cliente-carrinho.php">
+                                            <span class="mdi mdi-cart"></span><span class="ps-4"></span> Carrinho de compras</span></a></div>
+                                    <div class=" item-menu"><a class="nav-link " aria-current="page"
+                                                               href="./cliente-lista-desejos.php"><span class="mdi mdi-heart"></span><span class="ps-4"></span> Lista de Desejos</a></div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- <a class="nav-link" href="#">Link</a> -->
+                    <div class="item-menu">
+                        <a class="nav-link" href="#"><span class="mdi mdi-email item-menu-sub"></span><span
+                                class="ps-4 item-menu-sub">Mensagens</a>
+                    </div>
+                    <div class="item-menu">
+                        <a class="nav-link" href="#"><span class="mdi mdi-account"></span><span class="ps-4 ">Meu
+                                Perfil</a>
+                    </div>
+                </nav>
+
+            </div>
+            <!-- <div class="vh-100 d-none d-md-block"></div> -->
+        </div>
+
+    </nav>
+</div>
