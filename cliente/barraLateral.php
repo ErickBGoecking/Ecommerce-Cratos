6e1043108796dd926490648ce2d80cd5101bfade
Fernandodigitalarte <fernandodigitalarte@gmail.com>
2024-03-20 14:55:21 -0300

    alteração no menuLateral do adm e cliente

--- cliente/barraLateral.php
+++ cliente/barraLateral.php
@@ -1,91 +1,83 @@
-<div id="menuLateral" style="
-position:relative;
-    top: 0;
-    width: 100%;
-    z-index: 1000;
-">
-    <div class="barra_lateral col-md-2">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid align-items-start">
-                <a class="navbar-brand p-0 m-0" href="#"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse m-0 mt-3 " id="navbarTogglerDemo02">
+<div class="barra_lateral col-md-2" id="menuLateral">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid align-items-start">
+            <a class="navbar-brand p-0 m-0" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse m-0 mt-3 " id="navbarTogglerDemo02">
 
 
-                    <nav class="nav flex-column container-fluid justify-content-start p-0">
+                <nav class="nav flex-column container-fluid justify-content-start p-0">
 
-                        <div class="accordion accordion-flush container-fluid  p-0" id="accordionFlushExample">
-                            <div class="accordion-item">
-                                <div class=" item-menu">
-                                    <h2 class="accordion-header">
-                                        <div class="accordion-button collapsed bg-body-tertiary ps-0"
-                                            data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
-                                            aria-expanded="false" aria-controls="flush-collapseOne">
-                                            <span class="mdi mdi-shopping"></span><span class="ps-4"></span>Compras
-                                        </div>
-                                    </h2>
-                                </div>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-
-
-                                        <div class=" item-menu"><a class="nav-link" aria-current="page"
-                                                href="?pagina=historico"><span
-                                                    class="mdi mdi-order-bool-descending"></span><span
-                                                    class="ps-2"></span>Histórico</span></a></div>
-                                        <div class=" item-menu"><a class="nav-link" aria-current="page"
-                                                href="?pagina=carrinho">
-                                                <span class="mdi mdi-cart"></span><span class="ps-2"></span>
-                                                Carrinho</span></a></div>
-                                        <div class=" item-menu"><a class="nav-link " aria-current="page"
-                                                href="?pagina=lista-desejos"><span class="mdi mdi-heart"></span><span
-                                                    class="ps-2"></span> Lista de Desejos</a></div>
-
+                    <div class="accordion accordion-flush container-fluid  p-0" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <div class=" item-menu">
+                                <h2 class="accordion-header">
+                                    <div class="accordion-button collapsed bg-body-tertiary ps-0"
+                                        data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+                                        aria-expanded="false" aria-controls="flush-collapseOne">
+                                        <span class="mdi mdi-shopping"></span><span class="ps-4"></span>Compras
                                     </div>
+                                </h2>
+                            </div>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+
+
+                                    <div class=" item-menu"><a class="nav-link" aria-current="page"
+                                            href="?pagina=historico"><span
+                                                class="mdi mdi-order-bool-descending"></span><span
+                                                class="ps-2"></span>Histórico</span></a></div>
+                                    <div class=" item-menu"><a class="nav-link" aria-current="page"
+                                            href="?pagina=carrinho">
+                                            <span class="mdi mdi-cart"></span><span class="ps-2"></span>
+                                            Carrinho</span></a></div>
+                                    <div class=" item-menu"><a class="nav-link " aria-current="page"
+                                            href="?pagina=lista-desejos"><span class="mdi mdi-heart"></span><span
+                                                class="ps-2"></span> Lista de Desejos</a></div>
+
                                 </div>
                             </div>
                         </div>
+                    </div>
 
 
-                        <!-- <a class="nav-link" href="#">Link</a> -->
-                        <div class="item-menu">
-                            <a class="nav-link" href="#"><span class="mdi mdi-email item-menu-sub"></span><span
-                                    class="ps-4 item-menu-sub">Mensagens</a>
-                        </div>
-                        <div class="item-menu">
-                            <a class="nav-link" href="#"><span class="mdi mdi-account"></span><span class="ps-4 ">Meu
-                                    Perfil</a>
-                        </div>
-                    </nav>
+                    <!-- <a class="nav-link" href="#">Link</a> -->
+                    <div class="item-menu">
+                        <a class="nav-link" href="#"><span class="mdi mdi-email item-menu-sub"></span><span
+                                class="ps-4 item-menu-sub">Mensagens</a>
+                    </div>
+                    <div class="item-menu">
+                        <a class="nav-link" href="#"><span class="mdi mdi-account"></span><span class="ps-4 ">Meu
+                                Perfil</a>
+                    </div>
+                </nav>
 
-                </div>
-                <!-- <div class="vh-100 d-none d-md-block"></div> -->
             </div>
+            <!-- <div class="vh-100 d-none d-md-block"></div> -->
+        </div>
 
-        </nav>
-    </div>
+    </nav>
 </div>
 
-
 <script>
-window.addEventListener('scroll', function() {
-    var menu = document.getElementById('menuLateral');
+function mudar() {
+    var menu = document.getElementById("menuLateral");
+    var resolucao = window.innerWidth;
 
-    if (window.scrollY > 260) {
-        menu.style.position = 'fixed';
-        menu.style.top = '0';
-        menu.style.width = '100%';
-        menu.style.zIndex = '1';
-    }else{
-        
-        menu.style.position = 'relative';
-        menu.style.top = '0';
-        menu.style.width = '100%';
-        menu.style.zIndex = '1';
-    }});
+    if (window.scrollY > 260 && resolucao < 720) {
+        menu.style.cssText = "position: fixed; top: 0; width: 100%; z-index: 1;";
+    } else if (resolucao < 720) {
+        menu.style.cssText = "position: relative; top: 0; width: 100%; z-index: 0;";
+    } else {
+        menu.style.cssText = "position: relative; top: 0; width: 16%; z-index: 0;";
+    }
+}
+
+window.addEventListener('scroll', mudar);
+window.addEventListener('resize', mudar);
+
 </script>
