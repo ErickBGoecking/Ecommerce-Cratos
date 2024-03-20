4983c04b9b360298a2490b6a5017066bba2ea898
exclusivy <lcpyes@hotmail.com>
2024-03-20 20:47:12 -0300

    menuLateral

--- adm/menuLateral.php
+++ adm/menuLateral.php
@@ -1,66 +1,108 @@
+<?php
+if (isset($_GET['pagina']) && !empty($_GET['pagina'])) {
+    $pagina = $_GET['pagina'];
+} else {
+    $pagina = '';
+}
+?>
+<style>
+    .ativo {
+        background-color: red !important;
+        color: red;
+    }
+</style>
 <div class="barra_lateral col-md-2" id="menuLateral">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid align-items-start">
             <a class="navbar-brand p-0 m-0" href="#"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse m-0 mt-3 " id="navbarTogglerDemo02">
+
+
                 <nav class="nav flex-column container-fluid justify-content-start p-0">
-                    <div class="accordion accordion-flush container-fluid  p-0" id="menuAdm">
+
+                    <div class="accordion accordion-flush container-fluid  p-0" id="accordionFlushExample">
                         <div class="accordion-item">
-                            <div class="item-menu">
+                            <div class=" item-menu">
                                 <h2 class="accordion-header">
-                                    <a href="#">
-                                    <div class="accordion-button collapsed bg-body-tertiary ps-0" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        <span class="mdi mdi-content-save-move"></span><span class="ps-2"></span>Cadastros
+                                    <div class="accordion-button collapsed bg-body-tertiary ps-0"
+                                         data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
+                                         aria-expanded="false" aria-controls="flush-collapseOne">
+                                        <span class="mdi mdi-shopping"></span><span class="ps-4"></span>Compras
                                     </div>
-                                    </a>
                                 </h2>
                             </div>
-                            <div id="flush-collapseOne" class="accordion-collapse collapse show" data-bs-parent="#menuAdm">
+                            <div id="flush-collapseOne" class="accordion-collapse collapse
+                                <?php
+                            if ($pagina == 'historico' or $pagina == 'carrinho' or $pagina == 'lista-desejos') {
+                                echo 'show';
+                            }
+                            ?>"
+                                 data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body">
-                                    <div class="item-menu"><a class="nav-link" aria-current="page" href="?pagina=categoria"><span class="mdi mdi-order-bool-descending"></span><span class="ps-2"></span>Categoria</span></a></div>
-                                    <div class="item-menu"><a class="nav-link" aria-current="page" href="?pagina=historico"><span class="mdi mdi-order-bool-descending"></span><span class="ps-2"></span>Pettersen</span></a></div>
-                                    <div class="item-menu"><a class="nav-link" aria-current="page" href="?pagina=carrinho"><span class="mdi mdi-cart"></span><span class="ps-2"></span> Carrinho</span></a></div>
-                                    <div class="item-menu"><a class="nav-link " aria-current="page" href="?pagina=lista-desejos"><span class="mdi mdi-heart"></span><span class="ps-2"></span> Lista de Desejos</a></div>
+
+                                    <div class=" item-menu"><a class="nav-link <?php if ($pagina == 'historico') {
+                                            echo 'ativo';
+                                        } ?>" aria-current="page"
+                                                               href="?pagina=historico"><span
+                                                    class="mdi mdi-order-bool-descending"></span><span
+                                                    class="ps-2"></span>Histórico</span></a></div>
+                                    <div class=" item-menu"><a class="nav-link <?php if ($pagina == 'carrinho') {
+                                            echo 'ativo';
+                                        } ?>" aria-current="page"
+                                                               href="?pagina=carrinho">
+                                            <span class="mdi mdi-cart"></span><span class="ps-2"></span>
+                                            Carrinho</span></a></div>
+                                    <div class=" item-menu "><a class="nav-link  <?php if ($pagina == 'lista-desejos') {
+                                            echo 'ativo';
+                                        } ?>" aria-current="page"
+                                                                href="?pagina=lista-desejos"><span
+                                                    class="mdi mdi-heart"></span><span
+                                                    class="ps-2"></span> Lista de Desejos</a></div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
+
+
                     <!-- <a class="nav-link" href="#">Link</a> -->
                     <div class="item-menu">
                         <a class="nav-link" href="#"><span class="mdi mdi-email item-menu-sub"></span><span
-                                class="ps-2 item-menu-sub">Mensagens</a>
+                                    class="ps-4 item-menu-sub">Mensagens</a>
                     </div>
                     <div class="item-menu">
-                        <a class="nav-link" href="#"><span class="mdi mdi-account"></span><span class="ps-2 ">Meu
+                        <a class="nav-link" href="?pagina=perfil"><span class="mdi mdi-account"></span><span
+                                    class="ps-4 ">Meu
                                 Perfil</a>
                     </div>
                 </nav>
 
             </div>
+            <!-- <div class="vh-100 d-none d-md-block"></div> -->
         </div>
+
     </nav>
 </div>
 
-
 <script>
-function mudar() {
-    var menu = document.getElementById("menuLateral");
-    var resolucao = window.innerWidth;
+    function mudar() {
+        var menu = document.getElementById("menuLateral");
+        var resolucao = window.innerWidth;
 
-    if (window.scrollY > 260 && resolucao < 720) {
-        menu.style.cssText = "position: fixed; top: 0; width: 100%; z-index: 1;";
-    } else if (resolucao < 720) {
-        menu.style.cssText = "position: relative; top: 0; width: 100%; z-index: 0;";
-    } else {
-        menu.style.cssText = "position: relative; top: 0; width: 16%; z-index: 0;";
+        if (window.scrollY > 260 && resolucao < 720) {
+            menu.style.cssText = "position: fixed; top: 0; width: 100%; z-index: 1;";
+        } else if (resolucao < 720) {
+            menu.style.cssText = "position: relative; width: 100%;";
+        } else {
+            menu.style.cssText = "position: relative; width: 16%;";
+        }
     }
-}
 
-window.addEventListener('scroll', mudar);
-window.addEventListener('resize', mudar);
+    window.addEventListener('scroll', mudar);
+    window.addEventListener('resize', mudar);
 
 </script>
