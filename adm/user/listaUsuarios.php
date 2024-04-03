fea0f27ef1f98f4a1c28817454369625889e8e96
Fernandodigitalarte <fernandodigitalarte@gmail.com>
2024-04-02 21:18:31 -0300

    melhorando a segurança na paginação

--- adm/user/listaUsuarios.php
+++ adm/user/listaUsuarios.php
@@ -4,7 +4,9 @@
 }
 </style>
 
-<div id="conteudo"></div>
+<div id="conteudo">
+
+</div>
 
 <script src="js/scriptcrato.js"></script>
-<script>window.onload = function() {carregarDadosPaginacao('user/carregar_dados.php',1)};</script>
+<script>window.onload = function() {carregarDadosPaginacao('listarUsuario',1)};</script>
